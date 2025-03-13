9d0eca472df000003beb14f7dffd345dada5332a
Raja Akash Kumar <rajaakashk9@gmail.com>
2025-03-13 11:04:21 +0530

    added leadSlice

---
+++ src/Features/leadSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchLeads = createAsyncThunk("get/leads", async () => {
+  try {
+    const response = await axios.get(
+      "https://backend-mp-2.vercel.app/api/leads"
+    );
+    if (!response) {
+      console.log("Failed to get response");
+    }
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error.response?.data?.message || "Failed to fetch projects";
+  }
+});
+
+export const leadSlice = createSlice({
+  name: "leads",
+  initialState: {
+    leads: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchLeads.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchLeads.fulfilled, (state, action) => {
+      state.status = "success";
+      console.log(action.payload);
+      state.leads = action.payload;
+    });
+    builder.addCase(fetchLeads.rejected, (state) => {
+      state.status = "error";
+      state.error = action.errro.message;
+    });
+  },
+});
+
+export default leadSlice.reducer;
